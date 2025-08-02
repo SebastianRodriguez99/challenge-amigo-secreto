57dcc2f7962f17dba73ce1ce7f980a18eeeb86a0
Sebastian Rodriguez <sebarodriguez881@gmail.com>
2025-08-01 22:11:13 -0300

    hasta la parte donde se puede agregar el nombre y aparece en la consola

--- app.js
+++ app.js
@@ -1,1 +1,30 @@
-// El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+let nombres = [];
+// ingresan los nombres y los guarda anda saber en donde.
+
+
+function agregarAmigo (){
+//esto es para buscar el ID que hay en html al precionar el boton
+    let IdHtml = document.getElementById("amigo");
+    //se usa el value para que guarde lo que se pone en el casillero, osea guarda el valor
+    let nombre = IdHtml.value.trim();
+    
+    
+    
+    if (nombre === "") {
+        alert("no hay nada escrito"); return;
+
+        
+    }
+    
+   nombres.push(nombre)
+
+   IdHtml.value = "";
+   IdHtml.focus ();
+   console.log (nombres)
+
+}
+
+
+ 
+
+ 
